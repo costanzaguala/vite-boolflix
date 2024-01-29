7a4f44c064dbb3dd418c4dedd16cfb4c23b8c059
Costanza Guala <147189953+costanzaguala@users.noreply.github.com>
2024-01-29 21:27:29 +0100

    Initial commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,134 @@
+<script>
+export default {
+    data() {
+        return {
+             
+        };
+     },
+    methods: {
+
+    }
+}
+</script>
+
+<template> 
+
+    <header class="fixed-top">
+        <nav class="navbar">
+            <div class="container-fluid navbar-expand-lg">
+
+                <img src="../assets/img/logo-gobike.png" alt="logo">
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                
+                <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                    <div class="offcanvas-header">
+                        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    </div>
+
+                    <div class="offcanvas-body">
+
+                        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                            <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link active" href="#">About us</a>
+                            </li>
+
+                            <li class="nav-item dropdown">
+                            <a class="nav-link active" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Training
+                            </a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="#">Riding Lesson</a></li>
+                                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                                    <li><a class="dropdown-item" href="#">Safe Driving</a></li>
+                                    <li><a class="dropdown-item" href="#">Mountain Bike</a></li>
+                                    <li><a class="dropdown-item" href="#">Trail Drive</a></li>
+                                    <li><a class="dropdown-item" href="#">Pedaling</a></li>
+                                    <li><a class="dropdown-item" href="#">All Training</a></li>
+                                </ul>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link active" href="#">Packages</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link active" href="#">Blog</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link active" href="#">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="d-flex">
+                    <div class="button-upcoming-events d-flex align-items-center"> 
+                        <img src="../assets/img/helmet.png" alt="">  
+                        <p>
+                            Upcoming events
+                        </p>
+                        <i class="fa-solid fa-arrow-right"></i>
+                    </div>
+                </div>
+            
+
+
+            </div>
+        </nav> 
+    </header>
+
+</template>
+
+<style lang="scss" scoped>
+
+header {
+    background-color: white;
+}
+
+img {
+    width: 220px;
+}
+
+.navbar {
+    margin: 0 100px;
+}
+
+.navbar-expand-lg .offcanvas {
+flex-grow: 0;
+}
+
+.nav-item {
+    padding: 0 10px;
+}
+
+.button-upcoming-events {
+    background-color: black;
+    color: white;
+    padding: 20px;
+}
+
+.button-upcoming-events img {
+  filter: invert(100%);
+  width: 50px;
+}
+
+.button-upcoming-events > * {
+    margin: 0 5px;
+}
+
+p {
+    margin-bottom: 0;
+}
+
+
+
+
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,128 @@
+<script>
+export default {
+    data() {
+        return {
+             
+        };
+     },
+    methods: {
+
+    }
+}
+</script>
+
+<template>
+    <main>
+        <div id="carouselExampleFade" class="carousel slide carousel-fade">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="../assets/img/slider-bike-4.jpg" class="d-block w-100" alt="...">
+                    <div class="slider-text">
+                        <h1>
+                            Unforgettable <br>
+                            Cycling Experience
+                        </h1>
+                        <p>Learn cycling from the pros.</p>
+                        <a href="#">
+                            Learn More <i class="fa-solid fa-arrow-right"></i>
+                        </a>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="../assets/img/slider-bike-9.jpg" class="d-block w-100" alt="...">
+                    <div class="slider-text">
+                        <h1>
+                            Professional <br>
+                            Cycling Club
+                        </h1>
+                        <p>Learn cycling from the pros</p>
+                        <a href="#">
+                            Learn More <i class="fa-solid fa-arrow-right"></i>
+                        </a>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="../assets/img/slider-bike-12.jpg" class="d-block w-100" alt="...">
+                    <div class="slider-text">
+                        <h1>
+                            Learn Mountain Bike  <br>
+                            From The Expert
+                        </h1>
+                        <p>Learn cycling from the pros</p>
+                        <a href="#">
+                            Learn More <i class="fa-solid fa-arrow-right"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon rounded" aria-hidden="true">
+                    <i class="fa-solid fa-chevron-left"></i>
+                </span>
+                
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                <span class="carousel-control-next-icon rounded" aria-hidden="true">
+                    <i class="fa-solid fa-chevron-right"></i>
+                </span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+        <div class="text-center">
+            <h2>
+                Our Professional Cycling Trainings
+            </h2>
+            <p>
+                Learn from the mountain biking expert.
+            </p>
+            <div class="row">
+                <div class="col-12">
+                    
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
+
+<style lang="scss" scoped>
+.carousel-control-prev-icon, .carousel-control-next-icon{
+    background-color: black;
+    background-image: none;
+    cursor: pointer;
+    width: 60px;
+    height: 60px;
+    line-height: 60px;
+    font-size: 1.5rem;
+
+}
+
+.carousel-control-prev, .carousel-control-next{
+    height: 10%;
+    top:50%;
+    transform: translateY(-50%);
+}
+
+.slider-text{
+    position: absolute;
+    z-index: 999;
+    top: 30%;
+    left: 20%;
+    color: white;
+    font-weight: bold;
+    h1{
+        font-size: 3.5rem; 
+    }
+    p{
+        font-size: 1.1rem;
+        margin-bottom: 50px;
+    }
+    a{
+        text-decoration: none;
+        color: white;
+        background-color: black;
+        padding: 25px 50px;
+        margin-top: 50px;
+    }
+}
+</style>
