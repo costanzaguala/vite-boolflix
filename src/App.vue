2cb744b4d48c5fbcf62531a77437e85f6c589cf9
costanzaguala <costanzasilviaguala@gmail.com>
2024-01-29 23:00:02 +0100

    milestone 0 e 1 finita

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,14 @@
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
 import AppFooter from './components/AppFooter.vue';
-
+import axios from 'axios';
+import { store } from './store.js';
 
 
 export default {
     data() {
         return {
+            store,
              
         };
      },
@@ -16,22 +18,33 @@ export default {
     components: {
         AppHeader,
         AppMain,
-        AppFooter
     },
 
     methods: {
+        search() {
+            axios 
+                .get('https://api.themoviedb.org/3/search/movie', {
+                    params: {
+                        api_key: 'c1fb2d9cb791b1a49d30ff785e40f309',
+                        query: this.store.searchInput
+                    }
+                })
+
+                .then((response) => {
+                    console.log(response.data.results)
+                    this.store.movies=response.data.results; 
+                });
+        }
 
     }
 }
 </script>
 
-<template>
+<template> 
 
-    <AppHeader/>
+    <AppHeader @performSearch="search()"/>
 
     <AppMain/>
-
-    <AppFooter/>
 
 </template>
 
